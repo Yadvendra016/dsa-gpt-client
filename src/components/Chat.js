4fbdd0b3306ab3957bd8f47bf1fa96125729abc5
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2025-05-07 11:25:24 +0530

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -276,7 +276,7 @@ const Chat = () => {
 
       try {
         // You might want to unify these or use an environment variable
-        const response = await api.post("http://34.56.215.239:8000/chat", {
+        const response = await api.post("https://35.193.112.172.sslip.io/chat", {
           message: messageToSend,
           conversationId,
         });
